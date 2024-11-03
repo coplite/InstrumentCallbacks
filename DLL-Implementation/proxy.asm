51cd14015808568f901852f38fee1198a67db348
coplite <96002960+coplite@users.noreply.github.com>
2024-11-03 15:18:14 -0800

    Update proxy.asm

--- DLL-Implementation/proxy.asm
+++ DLL-Implementation/proxy.asm
@@ -2,19 +2,17 @@ section .text
 	extern Hooker
 	extern RtlCaptureContext
 	global proxy
-	
 proxy:	
-    ; credits to https://gist.github.com/esoterix/df38008568c50d4f83123e3a90b62ebb
     mov gs:[0x2e0], rsp ; Win10 TEB InstrumentationCallbackPreviousSp
     mov gs:[0x2d8], r10 ; Win10 TEB InstrumentationCallbackPreviousPc
 	
 	mov r10, rcx ; save rcx
 	sub rsp, 0x4d0 ; CONTEXT structure size
 	and rsp, -16 ; align rsp
-	mov rcx, rsp ; rsp to first param
+	mov rcx, rsp ; rsp to 1st param
 	call RtlCaptureContext ; capture the thread's context
 	
-	sub rsp, 32 ; shadow stack space
-	call Hooker ; call our func
+	sub rsp, 32 ; shadow stack
+	call Hooker ; call and hopefully not int 3
 	
-	int 3 ; we should not be here.
+	int 3 ; if this happens its ggs
