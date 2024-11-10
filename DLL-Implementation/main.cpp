cfb81e3f152ddd9b62c347c4a7667aed7fdca9cc
coplite <96002960+coplite@users.noreply.github.com>
2024-11-10 14:24:12 -0800

    Update main.cpp

--- DLL-Implementation/main.cpp
+++ DLL-Implementation/main.cpp
@@ -5,8 +5,9 @@
 #include <cstdint>
 #include <psapi.h>
 #include <tlhelp32.h>
+#include <fstream>
 
-// g++ -shared main.cpp proxy.obj -lkernel32 -lntdll -ldbghelp -lpsapi -o payload.dll
+// g++ -shared main.cpp proxy.obj -lkernel32 -lntdll -ldbghelp -lpsapi -masm=intel -o payload.dll
 
 #define ProcessInstrumentationCallback 0x28
 
@@ -23,6 +24,12 @@ typedef struct _PROCESS_INSTRUMENTATION_CALLBACK_INFORMATION
 
 static bool flag = false;
 
+HANDLE hConsole;
+char buffer[1024];
+const char* filename = "Trace.log";
+std::fstream file;
+unsigned int counter = 0;
+
 void Hooker(CONTEXT* ctx)
 {
 	uintptr_t pTEB = (uintptr_t)NtCurrentTeb();	
@@ -33,7 +40,7 @@ void Hooker(CONTEXT* ctx)
 	if(!flag)
 	{
 		flag = true;
-
+		counter++;
 		DWORD64 Displacement;
 		PSYMBOL_INFO SymbolInfo;
 		BYTE SymbolBuffer[sizeof(SYMBOL_INFO) + MAX_SYM_NAME] = {0};
@@ -49,40 +56,110 @@ void Hooker(CONTEXT* ctx)
 
 		if(SymbolLookupResult)
 		{
-			printf("Function: %s is located at 0x%p\n", SymbolInfo->Name, ctx->Rip-2);	
+			SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);			
+			sprintf((char*)buffer, "[+] Function: %s is located at [0x%p]\n", SymbolInfo->Name, ctx->Rip-2);
+			std::cout << buffer;
+			file << buffer;
+			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 		}
 		else
 		{
-			printf("Function: UNKOWN is located at 0x%p\n", ctx->Rip-2);
+			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);			
+			sprintf((char*)buffer, "[!] Function: UNKNOWN is located at [0x%p]\n", ctx->Rip-2);
+			std::cout << buffer;
+			file << buffer;
+			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 		}
+		
 		if(!((ctx->Rip)-2 > (DWORD64)hNtdll) && ((ctx->Rip)-2 < ((DWORD64)hNtdll + mi.SizeOfImage)))
 		{
-			printf("[-] Direct Syscall detected, performing immediate termination!!\n");
-			printf("Location:     0x%p\n", ctx->Rip-2);
-			printf("NtDLL Base:   0x%p\n", (DWORD64)hNtdll);
-			printf("NtDLL Offset: 0x%p\n", (DWORD64)hNtdll + mi.SizeOfImage);
+			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);			// i might change formatting of this a bit who knows			
+			sprintf((char*)buffer, "   \\__[-] Direct Syscall detected, performing immediate termination!!\n");
+			std::cout << buffer;
+			file << buffer;
+			
+			sprintf((char*)buffer, "    \\____[Location of syscall:     		0x%p]\n", ctx->Rip-2);
+			std::cout << buffer;
+			file << buffer;
+			
+			sprintf((char*)buffer, "     \\______[Ntdll base address:  	 	0x%p]\n", (DWORD64)hNtdll);
+			std::cout << buffer;
+			file << buffer;
+			
+			sprintf((char*)buffer, "      \\________[Ntdll end address: 		0x%p]\n", (DWORD64)hNtdll + mi.SizeOfImage);
+			std::cout << buffer;
+			file << buffer;
+			
+			sprintf((char*)buffer, "       \\________[Return Value:                                 0x%llx]\n", (ctx->Rax));
+			std::cout << buffer;
+			file << buffer;
+
+			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
 			exit(-1);
 		}
+		/*
+		else if(counter % 2 == 1) // counter is an odd number
+		{
+			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
+			sprintf((char*)buffer, "   \\__[-] Indirect Syscall detected, performing immediate termination!!\n");
+			std::cout << buffer;
+			file << buffer;
+			
+			sprintf((char*)buffer, "    \\________[Return Value:	0x%llx]\n", (ctx->Rax));
+			std::cout << buffer;
+			file << buffer;
+			
+			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+			exit(-1);
+		}
+		*/
 		else
 		{
-			printf("[+] Normal Syscall!!\n");
-		}
+			SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
 
+			sprintf((char*)buffer, "   \\__[+] Normal Syscall!!\n");
+			std::cout << buffer;
+			file << buffer;
+			
+			sprintf((char*)buffer, "    \\__[Return Value: 0x%llx]\n");
+			std::cout << buffer;
+			file << buffer;
+			
+			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		}
 		flag = false;
 	}
 	RtlRestoreContext(ctx, NULL);
 }
 
+
 bool __stdcall DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 {
-	static FILE *file = nullptr;
 	switch(dwReason)
 	{
 		case DLL_PROCESS_ATTACH:
 		{
+			hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+			if (hConsole == INVALID_HANDLE_VALUE)
+			{
+				std::cout << "[-] Unable to get std handle!!";
+				exit(-1);
+			}
 			SymSetOptions(SYMOPT_UNDNAME);
 			SymInitialize((HANDLE)-1, NULL, true);
-	
+			
+			file.open(filename, std::ios::out | std::ios::app);
+			
+			if (!file.is_open())
+			{
+				std::cerr << "Failed to open the file." << std::endl;
+				exit(-1);
+			}
+			
+			// start inline hooking
+			// hooking for indirect syscalls
+			// end hooking
 			NtSetInformationProcess NtSetInfoProc = (NtSetInformationProcess)GetProcAddress(LoadLibraryA("ntdll.dll"), "NtSetInformationProcess");
 			if(NtSetInfoProc == NULL)
 			{
@@ -100,6 +177,7 @@ bool __stdcall DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 		}
 		case DLL_PROCESS_DETACH:
 		{
+			file.close();
 			break;
 		}
 	}
